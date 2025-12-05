63f8104a406eae897f4d92a249dfed2692eef8f3
Lewis-W <lwaltonne@gmail.com>
2025-12-06 03:43:34 +1100

    Update init.lua

--- init.lua
+++ init.lua
@@ -702,7 +702,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        roslyn = {},
+        -- roslyn = {},
         remark_ls = {},
         tailwindcss = {},
         -- omnisharp = {},
